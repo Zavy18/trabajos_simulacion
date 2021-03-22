7c5e8daec49c5973bc4b4985f21c68eb0caa9218
Zavy18 <62628626+Zavy18@users.noreply.github.com>
2021-03-22 17:35:36 -0600

    modificacion para usar la funcion log

--- distribucion_exponencial/bin/distribucion_exponencial.dart
+++ distribucion_exponencial/bin/distribucion_exponencial.dart
@@ -1,7 +1,7 @@
 import 'dart:developer';
 import 'package:distribucion_exponencial/distribucion_exponencial.dart'
     as distribucion_exponencial;
-import 'dart:math';
+import 'dart:math' as math;
 
 List<double> numeros = [
   0.1199,
@@ -25,45 +25,16 @@ List<double> numeros = [
   0.1832,
   0.917
 ];
-List<double> logi = [
-  -2.121097217,
-  -1.246880305,
-  -0.0661398025,
-  -0.801400766,
-  -0.4001791037,
-  -0.5835755441,
-  -1.289530693,
-  -0.2407984866,
-  -0.1958932368,
-  -1.604450371,
-  -1.220440997,
-  -2.017406151,
-  -0.1745915107,
-  -0.4315524429,
-  -0.9349639971,
-  -2.909554577,
-  -2.528231775,
-  -0.2175343125,
-  -1.697176827,
-  -0.08664780673
-];
 
 void main(List<String> arguments) {
-  //Profe no supimos como usar la funcion logaritmo natural en drt ya buscamos en la documentacion de la API math:dart y nos manda error al intertar usarla
-  //Asi que optamos por hacer un arreglo con los logaritmo naturales de los numero y asi poderle entregar la practica y si nos pudiera ayudar con eso estaria muy bien.
-  //Dejamos comentado alguna formas que intentamo pero ninguna funciono.
   double lambda = 5;
-  //double logBase(num x, num base) => log(x) / log(base);
-  //double log10(num x) => log(x) / ln10;
-  //external double log(num x);    no sirve esto profe
-  distribucion(numeros, lambda, logi);
+  distribucion(numeros, lambda);
 }
 
-void distribucion(List<double> numeros, double lambda, List<double> logi) {
+void distribucion(List<double> numeros, double lambda) {
   List<double> resultado = new List(numeros.length);
   for (var i = 0; i < numeros.length; i++) {
-    //resultado[i] = -(1 / lambda) * double logBase(numeros[i],"e");
-    resultado[i] = -(1 / lambda) * logi[i];
+    resultado[i] = -(1 / lambda) * math.log(numeros[i]);
     print(resultado[i]);
   }
 }
--- erlang/bin/erlang.dart
+++ erlang/bin/erlang.dart
@@ -1,4 +1,5 @@
 import 'package:erlang/erlang.dart' as erlang;
+import 'dart:math' as math;
 
 List<double> numeros = [
   0.1199,
@@ -23,41 +24,15 @@ List<double> numeros = [
   0.917
 ];
 
-List<double> logar = [
-  -0.9763673311,
-  -0.1021504195,
-  1.078590083,
-  0.3433291198,
-  0.7445507822,
-  0.5611543417,
-  -0.1448008068,
-  0.9039313993,
-  0.948836649,
-  -0.4597204851,
-  -0.07571111118,
-  -0.8726762649,
-  0.9701383751,
-  0.713177443,
-  0.2097658887,
-  -1.764824691,
-  -1.383501889,
-  0.9271955733,
-  -0.5524469409,
-  1.058082079,
-];
-
 void main(List<String> arguments) {
   double lambda = 5;
-  herlang(numeros, logar, lambda);
+  herlang(numeros, lambda);
 }
 
-void herlang(List<double> numeros, List<double> logar, double lambda) {
+void herlang(List<double> numeros, double lambda) {
   List<double> resultado = new List(numeros.length);
   for (var i = 0; i < numeros.length; i++) {
-    //Profe no sapimos usar la funcion logaritmo natural en otro trabajo se explica mejor lo que intentamos hacer pero
-    //Cuando sepamos como se usa solamente es cambiar la multiplicacion por la funcion de logaritmo con el numero del arreglo en luigar de usar todo un arreglo
-    //con los logaritmo naturales de los numeros en este caso el arreglo es la multiplicacion de pi por el numero y se le saca el logaritmo natural
-    resultado[i] = -(1 / lambda) * logar[i];
+    resultado[i] = -(1 / lambda) * math.log((3.1416 * numeros[i]));
     print(resultado[i]);
   }
 }
