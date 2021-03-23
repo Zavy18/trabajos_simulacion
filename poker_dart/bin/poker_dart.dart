8911ac0305917dea23a53127b0b00503a552c7e3
Zavy18 <62628626+Zavy18@users.noreply.github.com>
2021-03-23 00:53:12 -0600

    poker

--- poker_dart/bin/poker_dart.dart
+++ poker_dart/bin/poker_dart.dart
@@ -1,8 +1,7 @@
 import 'package:poker_dart/poker_dart.dart' as poker_dart;
+import 'dart:math';
 
 void main(List<String> arguments) {
-  print('Hello world: ${poker_dart.calculate()}!');
-
   List<double> numeros = [
     0.7089585,
     0.0278870,
@@ -26,68 +25,124 @@ void main(List<String> arguments) {
     0.6304819
   ];
 
-  poker(numeros);
+  List<double> p = [0.0001, 0.0045, 0.009, 0.072, 0.108, 0.504, 0.3024];
+
+  poker(numeros, p);
 }
 
-void poker(List<double> numeros) {
+void poker(List<double> numeros, List<double> p) {
   List<String> nuevo = new List(numeros.length);
-  int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0, j = 0, k = 0;
-  int pares = 0, dos_pares = 0, tercia = 0, poker = 0, quintilla = 0;
+  double diferentes = 0,
+      pares = 0,
+      dos_pares = 0,
+      tercia = 0,
+      poker = 0,
+      quintilla = 0;
+  double pares_t = 0,
+      dosp_t = 0,
+      tercia_t = 0,
+      poker_t = 0,
+      quintilla_t = 0,
+      diferentes_t = 0;
+  double n_mayus = 20;
+  List<double> conteo = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  List<double> fe = new List(7);
+  double efe_e = 0, acumulado = 0, equiso = 0;
 
   for (var i = 0; i < numeros.length; i++) {
     nuevo[i] = (numeros[i]).toString();
-    print(nuevo[i]);
     for (var z = 2; z < 7; z++) {
       if (nuevo[i][z] == "0") {
-        a++;
+        conteo[0]++;
       }
       if (nuevo[i][z] == "1") {
-        b++;
+        conteo[1]++;
       }
       if (nuevo[i][z] == "2") {
-        c++;
+        conteo[2]++;
       }
       if (nuevo[i][z] == "3") {
-        d++;
+        conteo[3]++;
       }
       if (nuevo[i][z] == "4") {
-        e++;
+        conteo[4]++;
       }
       if (nuevo[i][z] == "5") {
-        f++;
+        conteo[5]++;
       }
       if (nuevo[i][z] == "6") {
-        g++;
+        conteo[6]++;
       }
       if (nuevo[i][z] == "7") {
-        h++;
+        conteo[7]++;
       }
       if (nuevo[i][z] == "8") {
-        j++;
+        conteo[8]++;
       }
       if (nuevo[i][z] == "9") {
-        k++;
+        conteo[9]++;
       }
     }
-    print(a);
-    print(b);
-    print(c);
-    print(d);
-    print(e);
-    print(f);
-    print(g);
-    print(h);
-    print(j);
-    print(k);
-    a = 0;
-    b = 0;
-    c = 0;
-    d = 0;
-    e = 0;
-    f = 0;
-    g = 0;
-    h = 0;
-    j = 0;
-    k = 0;
+
+    for (var j = 0; j <= 9; j++) {
+      if (conteo[j] == 0 || conteo[j] == 1) {
+        diferentes++;
+      }
+
+      if (conteo[j] == 2) {
+        pares++;
+      }
+
+      if (conteo[j] == 3) {
+        tercia++;
+      }
+
+      if (conteo[j] == 5) {
+        poker++;
+      }
+    }
+
+    if (diferentes == 10) {
+      diferentes_t++;
+    }
+    if (pares == 1) {
+      pares_t++;
+    }
+    if (pares == 2) {
+      dosp_t++;
+    }
+    if (tercia == 3) {
+      tercia_t++;
+    }
+    if (poker == 4) {
+      poker_t++;
+    }
+    for (var y = 0; y <= 9; y++) {
+      conteo[y] = 0;
+    }
+    diferentes = 0;
+    pares = 0;
+  }
+
+  for (var w = 0; w <= 6; w++) {
+    fe[w] = n_mayus * p[w];
+    if (fe[w] < 5) {
+      acumulado = acumulado + fe[w];
+      if (acumulado > 5) {
+        acumulado = acumulado - fe[w];
+      }
+    }
+  }
+
+  pares_t = pow((pares_t - fe[5]), 2) / fe[5];
+  diferentes_t = pow(diferentes_t - fe[6], 2) / fe[6];
+  acumulado = pow((1 - acumulado), 2) / acumulado;
+
+  equiso = pares_t + diferentes_t + acumulado;
+  print(equiso);
+  if (equiso < 5.99) {
+    print("No se rechaza H0");
+  } else {
+    print("Se rechaza H0");
   }
 }
